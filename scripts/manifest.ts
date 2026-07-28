1884ab6e74d0107397641c31559316b9fc7f8015
helloworldbugs <helloworldbugs@github.com>
2026-07-29 02:31:38 +0800

    release: v2.1.0

--- scripts/manifest.ts
+++ scripts/manifest.ts
@@ -35,7 +35,7 @@ const permissions = [
 const baseManifest: Manifest.WebExtensionManifest = {
   name: 'KeePass Tusk - Password Access and Autofill',
   short_name: 'KeePass Tusk',
-  version: '2025.3.23',
+  version: '2.1.0',
   description: 'Readonly KeePass password database integration for Chrome and Firefox',
   icons: {
     '16': '/assets/16x16.png',
