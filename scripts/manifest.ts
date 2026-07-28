313613bc0d5e489c0df77cd54a98616497ae8ebb
helloworldbugs <helloworldbugs@github.com>
2026-07-29 01:26:56 +0800

    fix: use host_permissions instead of optional_host_permissions to fix CORS

--- scripts/manifest.ts
+++ scripts/manifest.ts
@@ -78,6 +78,7 @@ function chromeManifestV3(): Manifest.WebExtensionManifest {
       scopes: ['https://www.googleapis.com/auth/drive.file'],
     },
     permissions,
+    host_permissions: hostPermissions,
     content_security_policy: {
       extension_pages: isDev
         ? // this is required on dev for Vite script to load
@@ -92,7 +93,6 @@ function chromeManifestV3(): Manifest.WebExtensionManifest {
     background: {
       service_worker: backgroundScript,
     },
-    optional_host_permissions: hostPermissions,
   });
 }
 
